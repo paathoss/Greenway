424f378624719bff81b223b2c2aa1a03629af178
Leonardo Peñalver <leo.pvr13@gmail.com>
2022-03-29 10:00:13 -0300

    Estructura Basica

--- script/main.js
+++ script/main.js
@@ -3,3 +3,13 @@ var punto = document.getElementsByClassName('.gm-style img')
 if (punto) {
     console.log("patata")
 }
+
+var cargarPagina = (id) => {
+    document.getElementById("homePage").style.display = "none"
+    document.getElementById("estacionesPage").style.display = "none"
+    document.getElementById("guiaPage").style.display = "none"
+    document.getElementById("aboutPage").style.display = "none"
+    document.getElementById("contactPage").style.display = "none"
+    document.getElementById(id).style.display = "block"
+  }
+  cargarPagina("contactPage")
